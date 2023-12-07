f9b26fc68711838c307bea23d25fd8384d39d538
fadouaabdoul <fadouaabdoulmoulah@gmail.com>
2023-12-07 16:11:23 +0800

    tuto #7 statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.blue, //<-- SEE HERE
-      title: Text('my first app'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('hello Fadoua'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        print("clicked");
-        //  logic of the button
+  home: Home(),
+)
+);
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue, //<-- SEE HERE
+        title: Text('my first app'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('hello Fadoua'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("clicked");
+          //  logic of the button
         },
-      child: Text('click'),
-    ),
-  ),
-));
+        child: Text('click'),
+      ),
+    );
+
+  }
+}
+
 
